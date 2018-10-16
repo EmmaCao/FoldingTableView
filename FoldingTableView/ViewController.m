ccacc20fa378f52711cdf1391b1a8e1a28a5c97c
曹琴 <caoqin@csike.com>
2018-10-16 15:37:11 +0800

    Initial Commit

---
+++ FoldingTableView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FoldingTableView
+//
+//  Created by xhand on 2018/10/16.
+//  Copyright © 2018年 EmmaCao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ FoldingTableView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FoldingTableView
+//
+//  Created by xhand on 2018/10/16.
+//  Copyright © 2018年 EmmaCao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
